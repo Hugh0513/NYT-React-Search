7e3058daae8bc5a6855aee561f9ea2b61cff7ca9
Ryosaku <hiwaryo@gmail.com>
2018-01-22 21:58:31 -0800

    heroku

---
+++ scripts/build-and-start.js
@@ -0,0 +1,9 @@
+console.log(__dirname);
+// const args = ["run build"];
+// const opts = { stdio: "inherit", cwd: __dirname + "../client", shell: true };
+// const childProcess = require("child_process");
+
+// childProcess.spawnSync("npm", args, opts);
+
+// const expressOpts = { stdio: "inherit", cwd: __dirname + "../", shell: true };
+// childProcess.spawnSync('npm', ['run server'], opts);
